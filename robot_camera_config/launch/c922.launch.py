c28ddde16aa8afa6a2d0f1474ce2453575af3dd6
Henrik Spilhaug Torkildsen <henrik.torkildsen@gmail.com>
2026-03-18 16:29:46 +0100

    debug robot_camera_config launch

--- robot_camera_config/launch/c922.launch.py
+++ robot_camera_config/launch/c922.launch.py
@@ -1,5 +1,8 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
 
 def generate_launch_description():
 

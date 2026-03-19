035460ddf384646ae5038f8ab1d3aa3fddf3ea99
Henrik Spilhaug Torkildsen <henrik.torkildsen@gmail.com>
2026-03-19 16:26:09 +0100

    debug servo_test.py

--- motor_control/motor_control/servo_test.py
+++ motor_control/motor_control/servo_test.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 
-from .Emakefun_MotorHAT import Emakefun_MotorHAT, Emakefun_Servo
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+import time
+import atexit
+
+# Lokale Emakefun-filer (samme mappe)
+from .Emakefun_MotorHAT import Emakefun_MotorHAT, Emakefun_DCMotor, Emakefun_Servo
+from .Emakefun_MotorDriver import PWM
+from .Emakefun_I2C import Emakefun_I2C
 import time
 mh = Emakefun_MotorHAT(addr=0x60)
 

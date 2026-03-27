1d3b8e45bef5a7b2e30394d0d868c44fa88587b6
EiHol <eirik@holm1.no>
2026-03-27 21:11:03 +0100

    adjusted servo angle

--- motor_control/motor_control/servo_test.py
+++ motor_control/motor_control/servo_test.py
@@ -24,10 +24,10 @@ class ServoTestNode(Node):
     def timer_callback(self):
         """Kjøres hver 1. sekund"""
         if self.position == 0:
-            angle = 50
+            angle = 0
             self.position = 1
         else:
-            angle = 180
+            angle = 45
             self.position = 0
 
         self.get_logger().info(f"Setter servo til {angle} grader...")

177b6f88e17abfae504c51f3c114507c4364ea89
Henrik Spilhaug Torkildsen <henrik.torkildsen@gmail.com>
2026-03-30 19:31:00 +0200

    removed foxglove from launch files

--- robot_camera_config/launch/c922.launch.py
+++ robot_camera_config/launch/c922.launch.py
@@ -4,12 +4,19 @@ from launch.substitutions import PathJoinSubstitution, TextSubstitution
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+    
+    camera_config = PathJoinSubstitution([
+        FindPackageShare("robot_camera_config"),
+        "config",
+        "c922.yaml"
+    ])
+
     return LaunchDescription([
         Node(
             package="v4l2_camera",
             executable="v4l2_camera_node",
-            name="camera",
-            namespace="",
+            name="c922_camera",
+            namespace="camera",
             output="screen",
             parameters=[{
                 "camera_info_url": "file:///opt/robot_ws/install/robot_camera_config/share/robot_camera_config/config/c922_camera_info.yaml",

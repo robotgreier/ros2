21a6054132c164220545e2ebaf3d0780bfd3a598
Eirik Holm <eirik@holm1.no>
2026-05-02 14:07:54 +0200

    updated outdated file name

--- python_snn_node/setup.py
+++ python_snn_node/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/python_snn_node/config', [
-        'config/weights.mem',
+        'config/weights_current.mem',
         ]),
     ],
     install_requires=['setuptools'],

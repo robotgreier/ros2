03c14a5188552f48199a7279db8cb29364ffda46
Eirik Holm <eirik@holm1.no>
2026-05-02 14:04:30 +0200

    removed unused params file

--- python_snn_node/setup.py
+++ python_snn_node/setup.py
@@ -10,7 +10,6 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/python_snn_node/config', [
-        'config/params.yaml',
         'config/weights.mem',
         ]),
     ],

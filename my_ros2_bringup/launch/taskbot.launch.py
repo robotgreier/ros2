a3b368c530b4b12943494cce0658e6b1d141d6fd
Gudmundur617 <guara1010@oslomet.no>
2026-05-04 17:17:56 +0200

    uart node deimplemented in launch file

--- my_ros2_bringup/launch/taskbot.launch.py
+++ my_ros2_bringup/launch/taskbot.launch.py
@@ -205,13 +205,6 @@ def generate_launch_description():
             name='dopamine_reward_node',
             output='screen'
         ),
-
-        Node(
-            package='uart',
-            executable='uart_node',
-            name='uart_node',
-            output='screen'
-        ),
     
 
     ])

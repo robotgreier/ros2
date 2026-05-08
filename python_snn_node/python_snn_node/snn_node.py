de75f931da170d6972faab643f01e8f8e30f5f05
Gudmundur617 <guara1010@oslomet.no>
2026-05-08 13:32:49 +0200

    reward node adjustment

--- python_snn_node/python_snn_node/snn_node.py
+++ python_snn_node/python_snn_node/snn_node.py
@@ -553,7 +553,9 @@ class SNNNode(Node):
         decision = "IDLE"
 
         if force_stop:
+
             decision = "STOP_PROXIMITY"
+
         elif winner_idx < 0:
             decision = "IDLE"
             cmd.linear.x = self.forward_speed
@@ -564,7 +566,7 @@ class SNNNode(Node):
                 cmd.angular.z = +self.turn_speed
                 decision = ACTION_NAMES[0]
             elif winner_idx == 1:    # FORWARD
-                cmd.linear.x = self.forward_speed
+                cmd.linear.x = -self.forward_speed
                 cmd.angular.z = 0.0
                 decision = ACTION_NAMES[1]
             elif winner_idx == 2:    # RIGHT

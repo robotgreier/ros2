bd8a363bd045e99b106d2dc044ace635ba3b3c38
Gudmundur617 <guara1010@oslomet.no>
2026-05-08 17:28:08 +0200

    params, fpga decoder

--- my_ros2_bringup/launch/taskbot_fpga.launch.py
+++ my_ros2_bringup/launch/taskbot_fpga.launch.py
@@ -177,7 +177,11 @@ def generate_launch_description():
             package='fpga_action_decoder',
             executable='fpga_action_decoder_node',
             name='fpga_action_decoder_node',
-            output='screen'
+            output='screen',
+            parameters=[{
+                'linear_speed': 0.05,
+                'angular_speed': 0.05,
+            }],
         ),
 
         # Task manager node to coordinate
